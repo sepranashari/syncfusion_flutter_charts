7aebaa1993984e8c314b1a77086ff4d4a7c81c87
Sepran Ashari <mt-sepran.ashari@tech.jago.com>
2024-02-21 20:17:08 +0700

    original from synfusion

---
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/sepranashari/fvm/versions/3.19.0"
+export "FLUTTER_APPLICATION_PATH=/Users/sepranashari/Documents/Projects/sepran-app/plugins/syncfusion_flutter_charts-23.1.36/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
